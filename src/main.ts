7c27c6c0522d339b1c164d128ceb4c66d9db28b6
QuentinHsu <xuquentinyang@gmail.com>
2023-09-26 23:13:30 +0800

    change(global): format code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-import './style.css'
+import "./style.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
