4b84bb95464342cc442f27527b54f4a37f57cc83
QuentinHsu <xuquentinyang@gmail.com>
2023-09-28 11:24:58 +0800

    feat(global): add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 
+import Pinia from '@/store/index'
+
 import App from './App.vue'
 import router from './router'
 
 import '@unocss/reset/tailwind.css'
 
 const app = createApp(App)
-app.use(router).mount('#app')
+app.use(router).use(Pinia).mount('#app')
